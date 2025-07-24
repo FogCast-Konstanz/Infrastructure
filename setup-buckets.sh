6c67f04ac5df105c72abe2d7c74692b949825f0f
Lukas Benner <lukasbenner@mailbox.org>
2025-07-24 22:44:57 +0200

    adapt setup script

--- setup-buckets.sh
+++ setup-buckets.sh
@@ -5,3 +5,14 @@ influx bucket create \
 --name WeatherData \
 --org ${DOCKER_INFLUXDB_INIT_ORG} \
 --retention 0
+
+influx bucket create \
+--name WeatherForecast \
+--org ${DOCKER_INFLUXDB_INIT_ORG} \
+--retention 0
+
+
+influx bucket create \
+--name benchmark_score \
+--org ${DOCKER_INFLUXDB_INIT_ORG} \
+--retention 0
